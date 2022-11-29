88abb56c1170456104dcca1e1f62add966b6ed93
Chad Gallati <chad@gallati.dev>
2022-11-28 19:01:03 -0500

    chore: run export and generate

--- contentful/types/TypeProject.ts
+++ contentful/types/TypeProject.ts
@@ -2,12 +2,12 @@ import * as CFRichTextTypes from "@contentful/rich-text-types";
 import * as Contentful from "contentful";
 
 export interface TypeProjectFields {
-  title: Contentful.EntryFields.Symbol;
-  slug: Contentful.EntryFields.Symbol;
-  content: CFRichTextTypes.Block | CFRichTextTypes.Inline;
-  excerpt: Contentful.EntryFields.Symbol;
-  coverImage: Contentful.Asset;
-  order: Contentful.EntryFields.Integer;
+    title: Contentful.EntryFields.Symbol;
+    slug: Contentful.EntryFields.Symbol;
+    content: CFRichTextTypes.Block | CFRichTextTypes.Inline;
+    excerpt: Contentful.EntryFields.Symbol;
+    coverImage: Contentful.Asset;
+    order?: Contentful.EntryFields.Integer;
 }
 
 export type TypeProject = Contentful.Entry<TypeProjectFields>;
