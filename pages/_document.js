41f5fe4786d71b3de5bb9809d957068ab9e08794
Chad Gallati <chadgallati@gmail.com>
2024-08-25 23:11:17 -0400

    feat: add vercel speed component (#7)

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,25 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Document() {
   return (
     <Html lang="en">
-        <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet"/>
-            <link rel="shortcut icon" href="/logo.svg"/>
-        </Head>
-        <body>
-        <Main/>
-        <NextScript/>
-        <Analytics/>
-        </body>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="shortcut icon" href="/logo.svg" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </Html>
   );
 }
