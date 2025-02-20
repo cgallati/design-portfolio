b9880f428f23c94edfd7f6b4cd99cfa4dac03b48
Chad Gallati <chadgallat@gmail.com>
2025-02-19 21:34:13 -0500

    fix: no controls

--- components/ProjectDetail/EmbeddedImage.tsx
+++ components/ProjectDetail/EmbeddedImage.tsx
@@ -16,7 +16,7 @@ export const EmbeddedImage = ({
   if (jkItsAVid) {
     return (
       <EmbeddedImageContainer>
-        <StyledVideo autoPlay controls muted playsInline>
+        <StyledVideo autoPlay muted playsInline>
           <source src={jkItsAVid} type="video/mp4" />
         </StyledVideo>
       </EmbeddedImageContainer>
