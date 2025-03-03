0852048f7772f2fb138673233e3dbdb0151def1a
Chad Gallati <chadgallat@gmail.com>
2025-03-03 15:24:37 -0500

    fix: more anis

--- components/ImageAndCaption/image-and-caption.tsx
+++ components/ImageAndCaption/image-and-caption.tsx
@@ -7,10 +7,15 @@ import {
   Caption,
 } from "./image-and-caption.styles";
 import { getMediaQueryIndex } from "../../lib/theme";
+import { useScrollAnimation } from "../../hooks/useScrollAnimation";
 
 export const ImageAndCaption: FC<TypeImageAndCaption> = ({ fields }) => {
   const { image, biggerImageAndGreyBg, caption } = fields;
   const [isMediumUp, setIsMediumUp] = useState(false);
+  
+  // Add scroll animations
+  const imageContainerRef = useScrollAnimation();
+  const captionRef = useScrollAnimation({ delay: 200 });
   
   useEffect(() => {
     // Initialize on mount
@@ -28,7 +33,10 @@ export const ImageAndCaption: FC<TypeImageAndCaption> = ({ fields }) => {
   return (
     <Container biggerImageAndGreyBg={biggerImageAndGreyBg}>
       {image && (
-        <ImageContainer biggerImageAndGreyBg={biggerImageAndGreyBg}>
+        <ImageContainer 
+          biggerImageAndGreyBg={biggerImageAndGreyBg}
+          ref={(el) => imageContainerRef.ref(el)}
+        >
           <Image
             src={"https:" + image.fields.file.url}
             alt={image.fields.description || ""}
@@ -38,7 +46,12 @@ export const ImageAndCaption: FC<TypeImageAndCaption> = ({ fields }) => {
           />
         </ImageContainer>
       )}
-      <Caption biggerImageAndGreyBg={biggerImageAndGreyBg}>{caption}</Caption>
+      <Caption 
+        biggerImageAndGreyBg={biggerImageAndGreyBg}
+        ref={(el) => captionRef.ref(el)}
+      >
+        {caption}
+      </Caption>
     </Container>
   );
 };
